a06ead6bd18d943853bedd9d6197239f4e3a3177
Piroska Werner <piroska.werner@gmail.com>
2019-10-31 12:49:38 -0500

    feat: Footer added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/**
+* Add Navigation to Home Page as soon as the page is loaded
+**/
+document.addEventListener('DOMContentLoaded', () => {
+    addNavigation();
+})
+
+
+/** 
+* Adds navigation link to page
+**/
+addNavigation = () => {
+    let navigationMenu = ['Home', 'About', 'Programs', 'Services', 'Testimonials', 'Contact'];
+    
+}
+
 
