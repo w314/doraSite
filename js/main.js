f49f0e8f59acf7bcd728d19c0be84758abaff60e
Piroska Werner <piroska.werner@gmail.com>
2019-11-07 11:22:21 -0600

    feat: Terms and conditions page added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 /**
-* Add Navigation to Home Page as soon as the page is loaded
+* Add event listeners when page is loaded
 **/
+
+// const path = window.location.pathname;
+// console.log(path);
+// const pos = path.lastIndexOf('/');
+// console.log(pos);
+// const page = path.slice(pos+1, -4)
+// console.log(page)
+
 document.addEventListener('DOMContentLoaded', () => {
-    addNavigation();
+    navMenu();
 })
 
 
 /** 
 * Adds navigation link to page
 **/
-addNavigation = () => {
+navMenu = () => {
+
+    const path = window.location.pathname;
+    console.log(path);
+    const pos = path.lastIndexOf('/');
+    console.log(pos);
+    const page = path.slice(pos+1, -4);
+    console.log(page);
+    const activeLinkElement = document.getElementById(page);
+    console.log(activeLinkElement);
+    activeLinkElement.classList.add('active')
+
     let navigationMenu = ['Home', 'About', 'Programs', 'Services', 'Testimonials', 'Contact'];
-    
 }
 
 
---
+++ terms.php
@@ -0,0 +1,4 @@
+<?php include './html/head.html' ?>
+<?php include './html/header.html' ?>
+<?php include './html/termsContent.html' ?>
+<?php include './html/footer.html.' ?>
